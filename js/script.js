f71faf4120a3b704d28a365db98343021b5b4c94
Techbiz2026 <muhammadabumunira@gmail.com>
2026-08-27 18:58:09 +0300

    Added About Us section

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
 const languageSelect = document.getElementById("languageSelect");
 
 if (languageSelect) {
-    languageSelect.addEventListener("change", function () {
 
+    // ھازىرقى بەتكە قاراپ تىل نامىنى توغرا كۆرسىتىش
+    const path = window.location.pathname;
+
+    if (path.includes("/tr/")) {
+        languageSelect.value = "tr";
+    } else if (path.includes("/en/")) {
+        languageSelect.value = "en";
+    } else if (path.includes("/ar/")) {
+        languageSelect.value = "ar";
+    } else {
+        languageSelect.value = "ug";
+    }
+
+    // تىل ئالماشتۇرۇش
+    languageSelect.addEventListener("change", function () {
         const language = this.value;
 
         if (language === "ug") {
             window.location.href = "/index.html";
-        }
-
-        if (language === "tr") {
+        } else if (language === "tr") {
             window.location.href = "/tr/index.html";
-        }
-
-        if (language === "en") {
+        } else if (language === "en") {
             window.location.href = "/en/index.html";
-        }
-
-        if (language === "ar") {
+        } else if (language === "ar") {
             window.location.href = "/ar/index.html";
         }
-
     });
 }
