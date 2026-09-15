5a448fbb215463b966806ea6671546dd72d274a8
techdilbiz <muhammadabumunira@gmail.com>
2026-09-15 20:24:06 +0300

    Fix mobile responsive layout

--- js/script.js
+++ js/script.js
@@ -2,34 +2,25 @@ const languageSelect = document.getElementById("languageSelect");
 
 if (languageSelect) {
 
-    // ھازىرقى بەتكە قاراپ تىل نامىنى توغرا كۆرسىتىش
     const path = window.location.pathname;
 
     if (path.includes("/tr/")) {
-        languageSelect.value = "tr";
-    } else if (path.includes("/en/")) {
-        languageSelect.value = "en";
-    } else if (path.includes("/ar/")) {
-        languageSelect.value = "ar";
-    } else {
-        languageSelect.value = "ug";
+        languageSelect.value = "/tr/index.html";
+    } 
+    else if (path.includes("/en/")) {
+        languageSelect.value = "/en/index.html";
+    } 
+    else if (path.includes("/ar/")) {
+        languageSelect.value = "/ar/index.html";
+    } 
+    else {
+        languageSelect.value = "/index.html";
     }
 
-    // تىل ئالماشتۇرۇش
     languageSelect.addEventListener("change", function () {
-        const language = this.value;
-
-        if (language === "ug") {
-            window.location.href = "/index.html";
-        } else if (language === "tr") {
-            window.location.href = "/tr/index.html";
-        } else if (language === "en") {
-            window.location.href = "/en/index.html";
-        } else if (language === "ar") {
-            window.location.href = "/ar/index.html";
-        }
+        window.location.href = this.value;
     });
-} 
+}
 // ===== MOBILE MENU =====
 const menuButton = document.querySelector(".menu-button");
 const navLinks = document.querySelector(".nav-links");
