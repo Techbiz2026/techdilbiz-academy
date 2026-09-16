ee89ee2ccc336915c9a3caf488ec7e60c813863a
techdilbiz <muhammadabumunira@gmail.com>
2026-09-16 21:58:56 +0300

    Fix mobile responsive layout

--- js/script.js
+++ js/script.js
@@ -21,12 +21,17 @@ if (languageSelect) {
         window.location.href = this.value;
     });
 }
+
+
 // ===== MOBILE MENU =====
+
 const menuButton = document.querySelector(".menu-button");
 const navLinks = document.querySelector(".nav-links");
 
 if (menuButton && navLinks) {
+
     menuButton.addEventListener("click", function () {
         navLinks.classList.toggle("show");
     });
+
 }
